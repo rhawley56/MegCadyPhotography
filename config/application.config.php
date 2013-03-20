e7e267bd388fffc98f93368481d8bddbc0d180fa
Reese Hawley <reese.hawley@gmail.com>
2013-03-20 00:11:38 -0500

    Adding SamUser Module for Doctrine User Authentication

--- config/application.config.php
+++ config/application.config.php
@@ -2,7 +2,15 @@
 return array(
     // This should be an array of module namespaces used in the application.
     'modules' => array(
-        'Application',
+	    'ZendDeveloperTools',
+	    'Application',
+	    'DoctrineModule',
+	    'DoctrineORMModule',
+	    'ZfcBase',
+	    'ZfcUser',
+	    'ZfcUserDoctrineORM',
+	    'BjyAuthorize',
+	    'SamUser',
     ),
 
     // These are various options for the listeners attached to the ModuleManager
